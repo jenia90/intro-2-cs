01321e7ee4fd0fe178bdd4b9fb0496f342749c46
Jenia <genia90@gmail.com>
2015-11-14 23:44:16 +0200

    Final pre-submit version

--- ex4/hangman_helper.py
+++ ex4/hangman_helper.py
@@ -3,6 +3,7 @@ import PIL.Image, PIL.ImageTk
 import random
 import threading
 import queue
+import os
 
 __HANGMAN_IMAGES__ = ['hangman0.png', 'hangman1.png','hangman2.png',\
                   'hangman3.png','hangman4.png','hangman5.png',\
@@ -39,12 +40,44 @@ def start_gui_and_call_main(main):
     #start the GUI:
     get_root().mainloop()
 
+
+class TKthread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        #self.daemon=True
+        self.start()
+
+    def callback(self):
+        os._exit(0)
+
+    def run(self):
+        get_display_obj()
+        self.root = get_root()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+        #label = tk.Label(self.root, text="Hello World")
+        #label.pack()
+        self.root.mainloop()
+
+def start_gui_and_call_main(main):
+    #Set up the GUI:
+    #get_display_obj()
+    #Start GUI in a new thread:
+    t = TKthread()
+
+    #run main:
+    while not __ROOT__:
+        pass
+    while not __DISPLAY__:
+        pass
+    main()
+
 def load_words(file='words.txt'):
     '''
     Loads a list of 58110 words from words.txt file
     '''
     words = []
-    f_words = open('words.txt')
+    f_words = open(file)
     for line in f_words:
         word = line.strip()
         if(word.isalpha()):
@@ -60,7 +93,7 @@ def get_random_word(words_list):
 
 def get_input():
     return get_display_obj().get_input()
-    
+
 
 def display_state(pattern, error_count, wrong_guess_lst,msg, ask_play=False):
     display = get_display_obj()
@@ -94,7 +127,7 @@ class Hangman():
         root.after(200,self.poll)
         root.minsize(width=700, height=400)
         self.main_container = tk.Frame(root, width=350, height=300)
-        self.main_container.pack(side="left", fill="both", expand=True)        
+        self.main_container.pack(side="left", fill="both", expand=True)
         self.left_frame = tk.Frame(self.main_container)
         self.left_frame.pack(side="left", fill="x", expand=False)
         self.right_frame = tk.Frame(self.main_container)
@@ -104,7 +137,7 @@ class Hangman():
         self.left_label = tk.Label(self.left_frame, bg='white')
         self.left_label.grid(row=0, column=0, sticky='W', padx=50, pady=50)
         self.l_pattern = tk.Label(self.right_frame, font=("Helvetica", 16), wraplength=400)
-        self.l_pattern.grid(row=0, column=0, padx=50, pady=50)        
+        self.l_pattern.grid(row=0, column=0, padx=50, pady=50)
         self.l_worng_guess_lst = tk.Label(self.right_frame,font=("Helvetica", 16), wraplength=400)
         self.l_worng_guess_lst.grid(row=1, column=0, padx=50, pady=20)
         self.choose_letter = tk.Frame(self.right_frame)
@@ -118,13 +151,14 @@ class Hangman():
         self.msg_label.grid(row=3, column=0, padx=50, pady=20)
         self.hint_b = tk.Button(self.right_frame, text="Get a hint!", width=20, command=self.callback_hint, font=("Helvetica", 12, "bold"))
         self.play_again_b = tk.Button(self.right_frame, text="Play Again!", width=20, command=self.callback_play_again, font=("Helvetica", 16, "bold"))
-        
+        self.play_again_b_f = tk.Button(self.right_frame, text="No More!", width=20, command=self.callback_play_again_f, font=("Helvetica", 16, "bold"))
+
         self.root.resizable(width=tk.FALSE, height=tk.FALSE)
-        
+
         self.input_queue = queue.Queue()
         self.task_queue = queue.Queue()
-        
-       
+
+
     def poll(self):
         while not self.task_queue.empty():
             task = self.task_queue.get()
@@ -145,14 +179,16 @@ class Hangman():
         self.left_label.config(image=photo)
         self.left_label.image = photo # keep a reference!
         self.l_pattern.config(text="Pattern: " + ' '.join(self.pattern))
-        self.l_worng_guess_lst.config(text="Wrong guesses: " + ','.join(self.wrong_guess_lst))              
-        if(self.ask_play):            
+        self.l_worng_guess_lst.config(text="Wrong guesses: " + ','.join(self.wrong_guess_lst))
+        if(self.ask_play):
             self.play_again_b.grid(row=5, column=0, padx=5, pady=5)
+            self.play_again_b_f.grid(row=10, column=0, padx=5, pady=5)
             self.hint_b.grid_remove()
             self.choose_letter.grid_remove()
         else:
             self.hint_b.grid(row=4, column=0, padx=5, pady=5)
             self.play_again_b.grid_remove()
+            self.play_again_b_f.grid_remove()
             self.choose_letter.grid(row=2, column=0, padx=5, pady=10)
         self.msg_label.config(text=self.msg, fg=self.msg_color)
 
@@ -174,18 +210,21 @@ class Hangman():
         self.input_queue.put((LETTER,letter))
         self.letter_entry.delete(0, 'end')
         self.update_gui()
-        
+
     def destroy(self):
         self.root.destroy()
-    
+
     def callback_quit(self):
         self.quit = True
         self.root.destroy()
-        
+
     def callback_hint(self):
-        self.input_queue.put((HINT,True))        
-        
+        self.input_queue.put((HINT,True))
+
     def callback_play_again(self):
         self.ask_play = False
         self.input_queue.put((PLAY_AGAIN,True))
-        
+
+    def callback_play_again_f(self):
+        self.ask_play = False
+        self.input_queue.put((PLAY_AGAIN,False))
