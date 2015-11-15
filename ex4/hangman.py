68bf5260acfdda6aad43c91f147b2873be6c676b
Jenia <genia90@gmail.com>
2015-11-16 00:10:51 +0200

    Final pre-submit version

--- ex4/hangman.py
+++ ex4/hangman.py
@@ -27,6 +27,12 @@ def update_word_pattern(word, pattern, letter):
 
 
 def match_pattern_to_word(pattern, word):
+    """
+    Checks if the given word matches the pattern
+    :param pattern: pattern as a string
+    :param word: word as a string
+    :return: returns True if the pattern matches and False if not
+    """
     if len(pattern) == len(word):
         for i in range(len(pattern)):
             if pattern[i] != word[i] and pattern[i] != UNDERSCORE:
@@ -123,41 +129,35 @@ def run_single_game(words_list):
     while UNDERSCORE in pattern and error_count < MAX_ERRORS:
         user_input = get_input()
 
-        if user_input[0] == LETTER:
-            if len(user_input[1]) > 1 or not user_input[1].islower():
+        if user_input[0] == LETTER:  # block of code to check validity of user input
+            if len(user_input[1]) > 1 or not user_input[1].islower():  # checks if single lower case letter was inputted
                 user_msg = NON_VALID_MSG
 
-            elif user_input[1] in wrong_guess_lst or user_input[1] in pattern:
+            elif user_input[1] in wrong_guess_lst or user_input[1] in pattern:  # checks if letter was inputted before
                 user_msg = ALREADY_CHOSEN_MSG + user_input[1]
 
-            elif user_input[1] in word:
-                pattern = update_word_pattern(word, pattern, user_input[1])
+            elif user_input[1] in word:  # if letter is valid updates the pattern and in case the word was guessed
+                pattern = update_word_pattern(word, pattern, user_input[1])         # stops the game
                 user_msg = DEFAULT_MSG
                 if pattern == word:
                     break
 
-            else:
+            else:   # if wrong guess adds the letter to the wrong guess list and increments the error counter
                 wrong_guess_lst.append(user_input[1])
                 error_count += 1
                 user_msg = DEFAULT_MSG
 
-            display_state(pattern, error_count, wrong_guess_lst, user_msg)
+            display_state(pattern, error_count, wrong_guess_lst, user_msg)  # updates GUI
 
-        elif user_input[0] == HINT:
+        elif user_input[0] == HINT:  # if asked for a hint gets the most common letter and shows it to the user
             hint_letter = choose_letter(filter_words_list(words_list, pattern, wrong_guess_lst), pattern)
             display_state(pattern, error_count, wrong_guess_lst, HINT_MSG + hint_letter)
 
-    if pattern != word:
+    if pattern != word:  # checks if the word wasn't guessed and shows lost game message
         display_state(pattern, error_count, wrong_guess_lst, LOSS_MSG + word, True)
 
-    else:
+    else:  # if game was won shows win message
         display_state(pattern, error_count, wrong_guess_lst, WIN_MSG, True)
-
-    user_input = get_input()
-
-    if user_input[0] == PLAY_AGAIN:
-        if user_input[1] == True:
-            run_single_game(words_list)
 
 
 def main():
@@ -166,7 +166,15 @@ def main():
     It initiates the game and passes the word list to the game engine.
     """
     words_list = load_words()
-    run_single_game(word_list)
+    run_single_game(words_list)
+
+    while True:
+        usr_inpt = get_input()
+        if usr_inpt[0] == PLAY_AGAIN:
+            if usr_inpt[1]:
+                run_single_game(words_list)
+            else:
+                break
 
 
 if __name__ == "__main__":
