5c31ba4ffa1495aaef017a6ee3f087eaaae774cb
Jenia <genia90@gmail.com>
2015-11-24 16:08:13 +0200

    Completed compare_pixel function

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -1,5 +1,19 @@
+import math
+
+RED_INDEX = 0
+GREEN_INDEX = 1
+BLUE_INDEX = 2
+
+
 def compare_pixel(pixel1, pixel2):
-    pass
+    """
+    compares color RGB values of 2 pixels
+    :param pixel1: tuple containing the RGB value of the first pixel
+    :param pixel2: tuple containing the RGB value of the second pixel
+    :return: returns the color difference of the pixels
+    """
+    return math.fabs(pixel1[RED_INDEX] - pixel2[RED_INDEX]) + math.fabs(pixel1[GREEN_INDEX] - pixel2[GREEN_INDEX]) + \
+           math.fabs(pixel1[BLUE_INDEX] - pixel2[BLUE_INDEX])
 
 
 def compare(image1, image2):
