68dab0c78bfb2bb95f2a1bc93e45f6b6158d6db8
Jenia <genia90@gmail.com>
2015-12-04 00:11:37 +0200

    finished steps 1a-1b

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -5,3 +5,34 @@
 #  DESCRIPTION : 
 #
 ##################################################
+
+
+def print_to_n(n):
+    """
+    prints all the numbers from 1 to n
+    :param n: last number to print
+    """
+    if n == 1:
+        print(n)
+    else:
+        print_to_n(n - 1)
+        print(n)
+
+
+def print_reversed(n):
+    """
+    prints all the numbers from n to 1
+    :param n: the first number to print
+    """
+    if n != 1:
+        print(n)
+        print_reversed(n-1)
+    else:
+        print(n)
+
+
+def is_prime(n):
+    pass
+
+
+print_to_n(10)
