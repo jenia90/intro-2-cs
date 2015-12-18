fa6fef5c039bc55abc470545191898e6e88e56fe
benfaingold <benfaingold@gmail.com>
2015-12-18 15:06:38 +0200

    Implemented getters and setters

--- ex9/ship.py
+++ ex9/ship.py
@@ -1,7 +1,10 @@
-INIT_HEADING = 0  # Parallel to the X axis
+from math import pi
 
 
 class Ship:
+    INIT_HEADING = 0  # Parallel to the X axis
+    DEG_TO_RAD = pi / 180
+
     def __init__(self, pos, vel):
         """
         Initializes a Ship object
@@ -12,4 +15,22 @@ class Ship:
         """
         self.pos = pos
         self.vel = vel
-        self.heading = INIT_HEADING
+        self.heading = self.INIT_HEADING
+
+    def get_position(self):
+        return self.pos
+
+    def get_velocity(self):
+        return self.vel
+
+    def get_heading_in_rad(self):
+        return self.heading * self.DEG_TO_RAD
+
+    def set_position(self, pos):
+        self.pos = pos
+
+    def set_velocity(self, vel):
+        self.vel = vel
+
+    def set_heading(self, heading):
+        self.heading = heading
