ebd521c7317c872c89384b715a0f42f68b0f3765
Jenia <genia90@gmail.com>
2015-12-23 00:21:25 +0200

    created Article and WikiNetwork class skeletons

--- ex10/ex10.py
+++ ex10/ex10.py
@@ -6,3 +6,67 @@
 #
 ##################################################
 
+
+def read_article_links(file_name='links_demo.txt'):
+    """
+    Reads a list of articles from a file and returns a tuple
+    :param file_name: file path as string
+    :return: list of tuples of articles
+    """
+    with open(file_name, 'r') as file:
+        return [tuple(article.strip() for article in articles.split('\t'))
+                for articles in file.readlines()]
+
+class Article:
+    """
+    Article object definition
+    """
+    def __init__(self, name):
+        self.name = name
+        self.article_collection = []
+
+    def get_name(self):
+        return self.name
+
+    def add_neighbor(self, neighbor):
+        pass
+
+    def get_neighbors(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __contains__(self, article):
+        pass
+
+class WikiNetwork:
+    """
+    WikiNetwork object definition
+    """
+    def __init__(self, link_list=[]):
+        self.link_lst = link_list
+
+    def update_network(self, link_list):
+        pass
+
+    def get_articles(self):
+        pass
+
+    def get_titles(self):
+        pass
+
+    def __contains__(self, article_name):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __getitem__(self, article_name):
+        pass
